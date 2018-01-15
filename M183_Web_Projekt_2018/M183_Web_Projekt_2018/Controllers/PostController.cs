566cb0dd065bf03f795d3a373ca797bcd0963ad9
Danimuheim <31336903+Danimuheim@users.noreply.github.com>
2018-01-15 22:05:46 +0100

    Auf der Startseite sollen alle veröffentlichten Posts (status published) angezeigt werden. --> fix

--- M183_Web_Projekt_2018/M183_Web_Projekt_2018/Controllers/PostController.cs
+++ M183_Web_Projekt_2018/M183_Web_Projekt_2018/Controllers/PostController.cs
@@ -16,25 +16,29 @@ namespace M183_Web_Projekt_2018.Controllers
         {
             SqlCommand cmd = GetSqlConnection();
             SqlDataReader reader;
-            cmd.CommandText = "SELECT [Id], [Title], [Description], [content] FROM [dbo].[Post]";
+            cmd.CommandText = "SELECT [Id], [Title], [Description], [content] FROM [dbo].[Post] WHERE DeletedOn IS Null";
             cmd.Parameters.Clear();
             cmd.Connection.Open();
             reader = cmd.ExecuteReader();
 
             var post = new Post();
-            while (reader.Read())
+            var list = new List<Post>();
+            if (reader.HasRows)
             {
-                post = new Post()
+                while (reader.Read())
                 {
-                    Id = reader.GetInt32(0),
-                    Title = reader.GetString(1),
-                    Description = reader.GetString(2),
-                    Content = reader.GetString(3),
-                };
+                    list.Add(new Post()
+                    {
+                        Id = reader.GetInt32(0),
+                        Title = reader.GetString(1),
+                        Description = reader.GetString(2),
+                        Content = reader.GetString(3),
+                    });
+                }
             }
-            cmd.Connection.Close();
+                cmd.Connection.Close();
 
-            return View(post);
+            return View(list);
 
         }
         [HttpPost]
