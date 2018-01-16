481e3ff0f8a127f448cd5eece63df4527dbc3414
Daniele Muheim <daniele.muheim@siemens.com>
2018-01-16 09:31:29 +0100

    Für jeden Post soll eine Detail-Ansicht erstellt werden. Ebenfalls sollen vorhandene Kommentare in der Detailansicht angezeigt werden.
    Es soll ein Formular für Kommentare in die Post-
    Detailansicht eingefügt werden.

--- M183_Web_Projekt_2018/M183_Web_Projekt_2018/Controllers/PostController.cs
+++ M183_Web_Projekt_2018/M183_Web_Projekt_2018/Controllers/PostController.cs
@@ -41,10 +41,69 @@ namespace M183_Web_Projekt_2018.Controllers
             return View(list);
 
         }
+        public ActionResult DetailPost(int id)
+        {
+            SqlCommand cmd = GetSqlConnection();
+            SqlDataReader reader;
+            cmd.CommandText = "SELECT Commet FROM [dbo].[Comment] WHERE PostId = " + id;
+            cmd.Parameters.Clear();
+            cmd.Connection.Open();
+
+            reader = cmd.ExecuteReader();
+
+            var list = new List<string>();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    list.Add(reader.GetString(0));
+                }
+            }
+            cmd.Connection.Close();
+            cmd.Connection.Open();
+            cmd.CommandText = "SELECT [Title], [Description], [content] FROM [dbo].[Post] WHERE Id = " + id;
+            reader = cmd.ExecuteReader();
+
+            var post = new Post();
+            while (reader.Read())
+            {
+                post = new Post()
+                {
+                    Title = reader.GetString(0),
+                    Description = reader.GetString(1),
+                    Content = reader.GetString(2),
+                    Id = id
+                };
+            }
+            cmd.Connection.Close();
+
+            post.Comments = list;
+            return View(post);
+        }
+
         [HttpPost]
         public ActionResult AddComment()
         {
-            return View();
+            var comment = Request["comment"];
+            var id = Request["id"];
+            if (!string.IsNullOrEmpty(comment) && comment.Length < 200)
+            {
+                SqlCommand cmd = GetSqlConnection();
+                cmd.CommandText = "INSERT INTO [dbo].[Comment] (commet, PostId) VALUES('" + comment + "', " + id + ")";
+                cmd.Parameters.Clear();
+                cmd.Connection.Open();
+                cmd.ExecuteReader();
+
+                ViewBag.Message = "";
+
+                cmd.Connection.Close();
+            }
+            else
+            {
+                ViewBag.Message = "Text ist zu lang oder ist leer!";
+            }
+
+            return DetailPost(int.Parse(id));
         }
         #region Private section
         private SqlCommand GetSqlConnection()
@@ -53,7 +112,7 @@ namespace M183_Web_Projekt_2018.Controllers
             SqlConnection con = new SqlConnection();
             SqlCommand cmd = new SqlCommand();
 
-            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\GitHub Project\\Data\\m183_project.mdf;Integrated Security=True;Connect Timeout=30";
+            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\M183_Project\\Data\\m183_project.mdf;Integrated Security=True;Connect Timeout=30";
             cmd.Connection = con;
 
             return cmd;
